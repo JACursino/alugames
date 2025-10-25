36a9db777ccaa5e8c5f54aaad66e1fcd26f1a283
jacursino <jacursino@gmail.com>
2025-10-24 21:14:55 -0300

    Altera - ajuste fino na function alterarStatus

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-function alterarStatus(idgame){
-    console.log("Clicou no jogo:", idgame);
+  function alterarStatus(numero){ /* Recebe o número do jogo como parâmetro */
+      // Montar o ID completo
+    let id = 'game-' + numero;  // Se passar 1, vira "game-1"
 
-}
+    let jogo = document.getElementById(id); /* Seleciona o elemento do jogo pelo ID */
+    console.log("Jogo encontrado:", jogo);
+
+    let imagem = jogo.querySelector(".dashboard__item__img"); /* Seleciona a imagem dentro do jogo */
+    console.log("Imagem encontrada:", imagem);
+
+    let botao = jogo.querySelector(".dashboard__item__button"); /* Seleciona o botão dentro do jogo */
+    console.log("Botão encontrado:", botao);
+    console.log("Texto do botão antes da alteração:", botao.textContent); /* Mostra o texto do botão antes da alteração */
+
+  }
